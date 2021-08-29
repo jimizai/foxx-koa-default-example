0369652e6fc3f33f24aa704c9a39b3a90b65ee30
dyk <359743984@qq.com>
2021-08-29 23:18:11 +0800

    chore: use better bootstrap mode

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,13 @@
-import { boostrap } from '@jimizai/core';
+import { createApp } from '@jimizai/core';
 import * as path from 'path';
 
-boostrap({
-  middlewares: [],
-  srcDirs: [path.resolve(__dirname, './app')],
-});
+async function main() {
+  const app = await createApp({
+    middlewares: [],
+    srcDirs: [path.resolve(__dirname, './app')],
+  });
+
+  app.bootstrap();
+}
+
+main();
